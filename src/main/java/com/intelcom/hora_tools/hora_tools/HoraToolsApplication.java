6c9abcc35827aeae40a4b99ed47311a779bc3985
Serge Oliveira Sardinha <serge.o.sardinha@intelcom.ca>
2025-12-09 14:32:36 -0500

    init project

---
+++ src/main/java/com/intelcom/hora_tools/hora_tools/HoraToolsApplication.java
@@ -0,0 +1,13 @@
+package com.intelcom.hora_tools.hora_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intelcom/hora_tools/hora_tools/HoraToolsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.intelcom.hora_tools.hora_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HoraToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
